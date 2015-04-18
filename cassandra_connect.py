955a0d68b3f884f88cd4ca4379b995bc7cea3497
Darren Jacques <djacques.ca@gmail.com>
2015-04-17 21:47:11 -0700

    count and inster tweets

--- cassandra_connect.py
+++ cassandra_connect.py
@@ -2,8 +2,25 @@ import ssl
 
 from cassandra.cluster import Cluster
 
-node_ip = '52.8.41.30'
+node_ip = '52.8.70.34'
 
 cluster = Cluster([node_ip])
 
-cluster.connect()
+session = cluster.connect('twitter')
+
+result = session.execute(
+    """
+    INSERT INTO tweets (id, screen_name, text, timestamp, url)
+    VALUES (%(id)s, %(screen_name)s, %(text)s, %(timestamp)s, %(url)s)
+    """,
+    {
+        'id': 30,
+        'screen_name': 'darren',
+        'text': 'darren\'s tweet',
+        'timestamp': 54523213,
+        'url': 'http://darren.com'
+    }
+)
+
+print(result)
+
--- twit.py
+++ twit.py
@@ -1,38 +1,64 @@
 from __future__ import absolute_import, print_function
 
+import json, yaml
+
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 
-import json, yaml
-
 from cassandra.cluster import Cluster
+
+
 
 with open('twitter_keys.yaml','r') as f:
     twitter_keys = yaml.load(f)
 
-cluster = Cluster(['52.8.41.30'])
+node_ip = '52.8.70.34'
+cluster = Cluster([node_ip])
 session = cluster.connect('twitter')
+
+
+
+def handle_insert_success(rows):
+    print("Successful insert")
+    print(rows)
+
+def handle_insert_error(exception):
+    log.error("Failed to insert tweet info: %s", exception)
 
 class StdOutListener(StreamListener):
     """ A listener handles tweets are the received from the stream.
     This is a basic listener that just prints received tweets to stdout.
     """
-    def on_data(self, data):
+    def __init__(self):
+        self.insert_statement = session.prepare(
+            """
+            INSERT INTO tweets (id, screen_name, text, timestamp, url)
+            VALUES (?, ?, ?, ?, ?)
+            """
+        )
+        self.counter = 0
 
+    def on_data(self, data):
         decoded = json.loads(data)
 
-        id = decoded['id']
-        time_stamp = decoded['created_at']
-        screen_name = decoded['user']['screen_name']
-        url = decoded['user']['url']
-        text = decoded['text'].encode('ascii', 'ignore')
-        #geo = decoded['geo']
+        id          = int(decoded['id'])
+        time_stamp  = int(decoded['timestamp_ms'])
+        screen_name = str(decoded['user']['screen_name'])
+        url         = str(decoded['user']['url'])
+        text        = str(decoded['text'].encode('ascii', 'ignore'))
+        #geo        = decoded['geo']
+
+        parameters = [ id , screen_name, text, time_stamp, url ]
+        session.execute(self.insert_statement, parameters)
+
+        self.counter += 1
+        print(self.counter)
 
         return True
 
-    def on_error(self, status):
-        print(status)
+def on_error(self, status):
+    print(status)
 
 if __name__ == '__main__':
     l = StdOutListener()
@@ -40,4 +66,5 @@ if __name__ == '__main__':
     auth.set_access_token(twitter_keys['access_token'], twitter_keys['access_token_secret'])
 
     stream = Stream(auth, l)
-    stream.filter(track=['basketball'])
+
+    stream.filter(track=['python', 'java', 'ruby', 'javascript'])
